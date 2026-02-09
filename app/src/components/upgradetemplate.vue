9db5ba3583f07f5b14c7892c7aef7a957068ee76
YH4-ES <yifanh4@nycstudents.net>
2026-02-09 14:01:43 -0500

    dfgh

---
+++ app/src/components/upgradetemplate.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{ upgrade.name }}</h2>
+        <p>{{ upgrade.desc }}</p>
+        <p>{{ upgrade.cost }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    upgrade: {
+        type: Object,
+        required: true,
+    }
+})
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- app/src/views/CookieClicker.vue
+++ app/src/views/CookieClicker.vue
@@ -1,13 +1,38 @@
 <template>
     <div id="cookiediv">
-        <h2>JOHNS</h2>
-        <img id="cookieimg"src="/cookie.png"/>
+        <h2>Cookies: {{ cookies }}</h2>
+        <img @click="incCookie()" id="cookieimg"src="/cookie.png"/>
     </div>
-    <div class="sidebar"></div>
+    <div class="sidebar"></div> <!--cycle through a list of stuff using a component-->
 </template>
 
 <script setup>
-    
+import {ref} from"vue"
+const cookies = ref(0)
+let clickInc = ref(1)
+function incCookie(){
+    cookies.value = cookies.value + clickInc.value
+}
+function changeInc(num){
+    clickInc.value = num
+}
+
+const upgrades = ref([
+    {
+        name : "Cookies-Per-Click",
+        cost : 0,
+        costInc: 2,
+        expo: 2,
+        upgradeLimit: 100
+    },
+    {
+        name : "Auto-Cookier",
+        cost : 5,
+        costInc: 10,
+        exp: 1.5,
+        upgradeLimit : 10
+    }
+])
 </script>
 
 <style scoped>
